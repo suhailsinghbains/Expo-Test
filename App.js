58f83f98e8166ecd61ee01eff15fc6b940d61186
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-02-09 09:15:48 +0530

    Calling custom API

--- App.js
+++ App.js
@@ -3,72 +3,45 @@ import { StyleSheet, ScrollView, ActivityIndicator, Text, View } from 'react-nat
 
 export default class App extends React.Component {
   state = {
-    loading: true,
-    error: false,
-    posts: [],
-  }
-
-  componentWillMount = async () => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-      const posts = await response.json()
-
-      this.setState({loading: false, posts})
-    } catch (e) {
-      this.setState({loading: false, error: true})
-    }
-  }
-
-  renderPost = ({id, title, body}, i) => {
-    return (
-      <View
-        key={id}
-        style={styles.post}
-      >
-        <View style={styles.postNumber}>
-          <Text>
-            {i + 1}
-          </Text>
-        </View>
-        <View style={styles.postContent}>
-          <Text>
-            {title}
-          </Text>
-          <Text style={styles.postBody}>
-            {body}
-          </Text>
-        </View>
-      </View>
-    )
-  }
-
-  render() {
-    const {posts, loading, error} = this.state
-
-    if (loading) {
-      return (
-        <View style={styles.center}>
-          <ActivityIndicator animating={true} />
-        </View>
-      )
-    }
-
-    if (error) {
-      return (
-        <View style={styles.center}>
-          <Text>
-            Failed to load posts!
-          </Text>
-        </View>
-      )
-    }
-
-    return (
-      <ScrollView style={styles.container}>
-        {posts.map(this.renderPost)}
-      </ScrollView>
-    )
-  }
+   loading: true,
+   error: false,
+   data: [],
+ }
+ componentWillMount = async () => {
+   try {
+     const response = await fetch('https://reqres.in/api/users?page=2')
+     const data = await response.json()
+     this.setState({loading: false, data})
+   } catch (e) {
+     this.setState({loading: false, error: true})
+   }
+ }
+ render() {
+   const {data, loading, error} = this.state
+   if (loading) {
+     return (
+       <View style={styles.center}>
+         <ActivityIndicator animating={true} />
+       </View>
+     )
+   }
+   if (error) {
+     return (
+       <View style={styles.center}>
+         <Text>
+           Failed to load posts!
+         </Text>
+       </View>
+     )
+   }
+   return (
+     <View style={styles.center}>
+       <Text>
+         {JSON.stringify(data["data"][0]["first_name"])}
+       </Text>
+     </View>
+   )
+ }
 }
 const styles = StyleSheet.create({
   container: {
